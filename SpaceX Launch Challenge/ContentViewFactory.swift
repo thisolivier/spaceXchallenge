636864b2050856b96cc3b881a3d726a7f59f5dd4
Olivier Butler <>
2021-11-12 13:09:56 +0000

    First commit

---
+++ SpaceX Launch Challenge/ContentViewFactory.swift
@@ -0,0 +1,8 @@
+//
+//  ContentViewFactory.swift
+//  SpaceX Launch Challenge
+//
+//  Created by Olivier Butler on 12/11/2021.
+//
+
+import Foundation
---
+++ SpaceX Launch Challenge/SpaceX_Launch_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceX_Launch_ChallengeApp.swift
+//  SpaceX Launch Challenge
+//
+//  Created by Olivier Butler on 10/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SpaceX_Launch_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
